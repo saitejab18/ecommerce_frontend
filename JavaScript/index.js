34fe83dc6032e3175151fd3bf75ad3c39a20b8c9
Saiteja <saitejasai3579@gmail.com>
2023-08-08 22:54:35 +0530

    v1

---
+++ JavaScript/index.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <script>
+            function  f1() 
+            {
+                var a = 10;
+                var b = 20;
+                console.log(a);
+                console.log(b);
+
+            &rbrace;
+        </script>
+    </body>
+</html>
